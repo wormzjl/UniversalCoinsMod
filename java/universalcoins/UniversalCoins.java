134171fabb39647d7fcf1d462f671ccc6aabf589
Steve <the.steve.carlson@gmail.com>
2014-10-10 17:37:31 -0700

    revert commands because they won't work in 1.7.10. More research needed.

--- java/universalcoins/UniversalCoins.java
+++ java/universalcoins/UniversalCoins.java
@@ -70,7 +70,7 @@ public class UniversalCoins {
 	public static UniversalCoins instance;
 	public static final String modid = "universalcoins";
 	public static final String name = "Universal Coins";
-	public static final String version = "1.7.2-1.5.6";
+	public static final String version = "1.7.2-1.5.7";
 	
 	public static Boolean autoModeEnabled;
 	public static Boolean updateCheck;
